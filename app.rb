acd70010360dee6f6d04bc040e84fe3e99fb478b
ks1venberg <venberg.dev@gmail.com>
2018-05-08 07:30:53 +0300

    copy_base files from prev_project

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+end
+
+class Barber < ActiveRecord::Base
+end
+
+
+before do
+	@barbers = Barber.all
+end
+
+get '/' do
+	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phome]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+end
